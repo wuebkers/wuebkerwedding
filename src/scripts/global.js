6176735917dc747022d1595763f7a161918d3118
wuebkers <skylar@skylarwuebker.com>
2019-02-03 14:28:46 -0500

    prefects animations

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -8,9 +8,9 @@ class Global {
 		$(window).on('scroll', function () {
 
 			var scrollPercent = $(document).scrollTop() / 80;
-			var scalePercent = ($(document).scrollTop() / 3000) + 1;
+			var scalePercent = ($(document).scrollTop() / 30) + 100;
 			var opacityPercent = 1 - $(document).scrollTop() / 1000;
-			$('.hero__media').attr('style', 'filter: blur(' + scrollPercent + 'px); transform: scale(' + scalePercent + '); opacity:' + opacityPercent + ';');
+			$('.hero__media').attr('style', 'filter: blur(' + scrollPercent + 'px); background-size: ' + scalePercent + '%;');
 		});		
 	}	
 }
