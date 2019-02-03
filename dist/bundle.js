0fdf14830166750c63b1dcd059db80218d9a3623
wuebkers <skylar@skylarwuebker.com>
2019-02-03 13:39:15 -0500

    makes responsive

--- dist/bundle.js
+++ dist/bundle.js
@@ -21,7 +21,7 @@ var slider = function () {
 
 	return slider;
 }();
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -33,35 +33,8 @@ var Global = function () {
 	}
 
 	_createClass(Global, [{
-		key: 'init',
-		value: function init() {
-			var dir = window.location + 'dist/media/home/slider';
-			var fileextension = ".jpg";
-
-			console.log(window.location);
-
-			setTimeout(function () {
-				$.ajax({
-					//This will retrieve the contents of the folder if the folder is configured as 'browsable'
-					url: dir,
-					success: function success(data) {
-						//List all .png file names in the page
-						$(data).find('a:contains(' + fileextension + ')').each(function () {
-							var filename = this.href.replace(window.location, '').replace('http://', '');
-							$('.slide-wrapper').append('<div class="slide" style="background - image: url("' + dir + '/' + filename + '"></div >');
-						});
-					}
-				});
-			}, 1000);
-
-			// $('.slide-wrapper').slick({
-			// 	slidesToShow: 1,
-			// 	dots: true,
-			// 	fade: true,
-			// 	autoplay: true,
-			// 	autoplaySpeed: 2000
-			// });
-		}
+		key: "init",
+		value: function init() {}
 	}]);
 
 	return Global;
--- src/scripts/global.js
+++ src/scripts/global.js
@@ -4,34 +4,7 @@ class Global {
 	}
 
 	init() {
-		var dir = window.location + 'dist/media/home/slider';
-		var fileextension = ".jpg";
-
-		console.log(window.location);
-
-		setTimeout(() => {
-			$.ajax({
-				//This will retrieve the contents of the folder if the folder is configured as 'browsable'
-				url: dir,
-				success: function (data) {
-					//List all .png file names in the page
-					$(data).find('a:contains(' + fileextension + ')').each(function () {
-						var filename = this.href.replace(window.location, '').replace('http://', '');
-						$('.slide-wrapper').append('<div class="slide" style="background - image: url("' + dir + '/' + filename + '"></div >');
-					});
-				}
-			});
-		}, 1000);
-		
-
-
-		// $('.slide-wrapper').slick({
-		// 	slidesToShow: 1,
-		// 	dots: true,
-		// 	fade: true,
-		// 	autoplay: true,
-		// 	autoplaySpeed: 2000
-		// });
+				
 	}	
 }
 
